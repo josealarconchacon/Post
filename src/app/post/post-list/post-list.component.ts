72397cbdbba812d24100dc4d834f60342b3eccf2
josealarconchacon <jose.alarcon.chacon@gmail.com>
2023-08-30 17:10:48 -0400

    loop and display dummy data for testing

--- src/app/post/post-list/post-list.component.ts
+++ src/app/post/post-list/post-list.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.css']
+  styleUrls: ['./post-list.component.css'],
 })
 export class PostListComponent {
+  // dummy data test
+  // postList = [
+  //   { title: 'Post 1', content: 'Content of post #1' },
+  //   { title: 'Post 2', content: 'Content of post #2' },
+  //   { title: 'Post 3', content: 'Content of post #3' },
+  // ];
 
+  postList = [];
 }
